b7da970cc9edcf54706a24895010be3e619feb66
Academicoff <academic.official.inf@gmail.com>
2022-01-21 22:31:17 +0300

    add 9. Math tasks

---
+++ 9. Math/script.js
@@ -0,0 +1,43 @@
+// 1. Даны длины трёх сторон треугольника. Определить,
+//     является ли треугольник прямоугольным.
+// 2. Пользователь вводит число R. Написать программу,
+//     которая выведет в консоль длину окружности и
+// площадь круга с радиусом R.
+// 3. *Пользователь вводит числа a, b и c. Написать
+// программу, выводящую корни квадратного
+// уравнения с коэффициентами a, b и c.
+
+
+// 1
+let a = 7;
+let b = 5;
+let c = 10;
+
+//квадрат гипотенузы равен сумме квадратов катетов
+let result = (Math.pow(a,2)+Math.pow(b,2)===Math.pow(c,2)) ? "Прямоугольный" : "Не прямоугольный";
+console.log(result);
+
+//2
+const input = +prompt("Введите R-адиус круга: ");
+const circleLength = 2 * Math.PI * input;
+const circleSquare = Math.PI * Math.pow(input,2);
+console.log(`Площадь круга с радиусом ${Math.round(input)} равна: ${Math.round(circleSquare)}. Длина окружности равна: ${Math.round(circleLength)}`);
+
+//3
+const abc = prompt("Введите коэффициенты a,b,c для квадратного уровнения, через запятую: ");
+[a, b, c] = abc.split(',');
+// ax2 + bx + c = 0 => D = b2 - 4ac
+console.log(`Уравнение: ${a}x2 + ${b}x + ${c} = 0`);
+const discr = Math.pow(b,2)-4*a*c;
+console.log(`Дискриминант D: ${discr}`);
+
+if (discr>0){
+    const x1 = (-b + Math.sqrt(discr)) / (2 * a);
+    const x2 = (-b - Math.sqrt(discr)) / (2 * a);
+    console.log(`x1= ${x1}, x2= ${x2}`);
+} else if (discr===0){
+    const x = -b / (2 * a);
+    console.log(`x= ${x}`);
+} else {
+    console.log("У данного уравнения нет корней!");
+}
