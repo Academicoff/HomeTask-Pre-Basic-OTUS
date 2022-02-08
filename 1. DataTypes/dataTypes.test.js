205458cd26077ff57032a33e7f34f4a419230e4a
Academicoff <academic.official.inf@gmail.com>
2022-02-09 02:46:44 +0300

    add jest and babel-jest

---
+++ 1. DataTypes/dataTypes.test.js
@@ -0,0 +1,7 @@
+import {sum} from "../1. DataTypes/first";
+
+describe("DataTypes tests", () => {
+    it("first task tests",()=>{
+        expect
+    })
+})
--- 1. DataTypes/first.js
+++ 1. DataTypes/first.js
@@ -1,3 +1,15 @@
-let a = 10;
-let b = 20;
-console.log("a * b = " + a * b + "| a + b = " + (a + b));
+// 1. В переменных a и b хранятся числа. Написать
+// программу, которая выводит в консоль произведение
+// и сумму этих чисел.
+//
+// let a = 10;
+// let b = 20;
+// console.log("a * b = " + a * b + "| a + b = " + (a + b));
+
+export function sum(a, b){
+    return a + b;
+}
+export function product(a, b){
+    return a * b;
+}
+console.log("a * b = " + product(a, b) + "| a + b = " + sum(a, b));
